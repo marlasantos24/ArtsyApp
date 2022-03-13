d4a167970adebd641ed30a43a2c602cc4ab44b30
MarlaSantos <72895099+marlasantos24@users.noreply.github.com>
2022-03-13 13:01:24 -0500

    ObrasPablo

--- app/src/main/java/com/example/artsyapp/Obral3.kt
+++ app/src/main/java/com/example/artsyapp/Obral3.kt
@@ -1,18 +1,17 @@
 package com.example.artsyapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class ObrasLeonardo : AppCompatActivity() {
+class Obral3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_obras_leonardo)
-        val bvolver= findViewById<Button>(R.id.bvolver1)
-        bvolver.setOnClickListener {
+        setContentView(R.layout.activity_obral3)
+
+        val bRegresar1= findViewById<Button>(R.id.regresar1)
+        bRegresar1.setOnClickListener {
             finish()
         }
-
     }
 }
--- app/src/main/java/com/example/artsyapp/ObrasFrancisco.kt
+++ app/src/main/java/com/example/artsyapp/ObrasFrancisco.kt
@@ -13,5 +13,6 @@ class ObrasFrancisco : AppCompatActivity() {
         bvolver.setOnClickListener {
             finish()
         }
+
     }
 }
