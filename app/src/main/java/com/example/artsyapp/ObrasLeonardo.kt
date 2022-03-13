4967d603c6d18e6cb1bcc2376fb5244350ebf6ec
MarlaSantos <72895099+marlasantos24@users.noreply.github.com>
2022-03-13 13:46:27 -0500

    ObrasLeonardo

--- app/src/main/java/com/example/artsyapp/ObrasLeonardo.kt
+++ app/src/main/java/com/example/artsyapp/ObrasLeonardo.kt
@@ -13,6 +13,35 @@ class ObrasLeonardo : AppCompatActivity() {
         bvolver.setOnClickListener {
             finish()
         }
-
+        val bObraf1= findViewById<Button>(R.id.descripcionobraf1)
+        bObraf1.setOnClickListener {
+            var intent = Intent(this, Obral1::class.java)
+            startActivity(intent)
+        }
+        val bObraf2= findViewById<Button>(R.id.descripcionobraf2)
+        bObraf2.setOnClickListener {
+            var intent = Intent(this, Obral2::class.java)
+            startActivity(intent)
+        }
+        val bObraf3= findViewById<Button>(R.id.descripcionobraf3)
+        bObraf3.setOnClickListener {
+            var intent = Intent(this, Obral3::class.java)
+            startActivity(intent)
+        }
+        val bObraf4= findViewById<Button>(R.id.descripcionobraf4)
+        bObraf4.setOnClickListener {
+            var intent = Intent(this, Obral4::class.java)
+            startActivity(intent)
+        }
+        val bObraf5= findViewById<Button>(R.id.descripcionobraf5)
+        bObraf5.setOnClickListener {
+            var intent = Intent(this, Obral5::class.java)
+            startActivity(intent)
+        }
+        val bObraf6= findViewById<Button>(R.id.descripcionobraf6)
+        bObraf6.setOnClickListener {
+            var intent = Intent(this, Obral6::class.java)
+            startActivity(intent)
+        }
     }
 }
